00d0d2fda6a67662203154ef3e2b70cf71af0b95
SyedaZuwaina <77491214+SyedaZuwaina@users.noreply.github.com>
2021-01-19 23:55:57 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,13 +1,14 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root');
+$server = "localhost";
+$username = "root";
+$password = "";
 
-mysqli_select_db($con, 'my_database');
-if($con){
-    //echo "connected";
-}
-else{
-    //echo "Not connected";
-}
+    $con = mysqli_connect($server, $username, $password);
+    $db = mysqli_select_db( $con, 'my_database');
 
+    if(!$con){
+      die("Connection to this database failed due to" .
+mysqli_connect_error());
+    }
 ?>
