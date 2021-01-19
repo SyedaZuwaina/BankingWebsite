2396994a78fd5a0adc7d3ad0df59ec66c6030fc6
SyedaZuwaina <77491214+SyedaZuwaina@users.noreply.github.com>
2021-01-19 23:29:54 +0530

    Update History.php

--- History.php
+++ History.php
@@ -16,8 +16,8 @@
 <div class="table-responsive">
 
 <div id="navbar">
-  <a class="one" href="Homepage.php">Home</a>
-  <a class="two" href="index.php">View Customer</a>
+  <a class="one" href="index.php">Home</a>
+  <a class="two" href="Display.php">View Customer</a>
   <a class="three" href="History.php">Transaction History</a>
   </div>
   
